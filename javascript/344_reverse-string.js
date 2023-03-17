7d67fb67f737e8187e75953523a0358364385ff4
Lee <jessy1092@gmail.com>
2023-03-17 19:55:44 +0800

    fix: Use recursive to resolve 344_reverse-string.js

--- javascript/344_reverse-string.js
+++ javascript/344_reverse-string.js
@@ -3,9 +3,23 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-  for (let i = 0; i < s.length / 2; i++) {
-    let tmp = s[i];
-    s[i] = s[s.length - 1 - i];
-    s[s.length - 1 - i] = tmp;
-  }
+  recursiveReverseString(s, 0);
 };
+
+var recursiveReverseString = function (s, i) {
+  if (i >= s.length / 2) {
+    return;
+  }
+
+  [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]];
+
+  recursiveReverseString(s, i + 1);
+};
+
+// var reverseString = function (s) {
+//   for (let i = 0; i < s.length / 2; i++) {
+//     let tmp = s[i];
+//     s[i] = s[s.length - 1 - i];
+//     s[s.length - 1 - i] = tmp;
+//   }
+// };
